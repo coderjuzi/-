1083379cc56a80da5e4f185c48eaff482708565a
橘子Vincent <vincent.juzihao@gmail.com>
2019-10-12 12:55:49 +0800

    首页模板封装完成

---
+++ data/home-data.js
@@ -0,0 +1,110 @@
+let home_data = [
+
+  {
+    name: '盗将行',
+    date:'2019年10月11日',
+    type: '本格',
+    number: '6',
+    time: '5小时',
+    img: '/images/carousel/post001.jpg',
+    avatar: '/images/avatar/coderjuzi.png',
+    author: '桔推理编辑部',
+    content: '盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行盗将行',
+    star: '/images/icon/star/star-4.png',
+    score: '7.8',
+    favour_image: '/images/icon/chat.png',
+    favour_count: 88,
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    name: '年轮',
+    date:'2019年10月12日',
+    type: '变格',
+    number: '8',
+    time: '4小时',
+    img: '/images/carousel/post002.jpg',
+    avatar: '/images/avatar/coderjuzi.png',
+    author: '桔推理编辑部',
+    content: '年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮年轮',
+    star: '/images/icon/star/star-5.png',
+    score: '9.4',
+    favour_image: '/images/icon/chat.png',
+    favour_count: 15,
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    name: '蒙轻纱的处女',
+    date:'2019年9月13日',
+    type: '变格',
+    number: '7',
+    time: '5小时',
+    img: '/images/carousel/post003.jpg',
+    avatar: '/images/avatar/coderjuzi.png',
+    author: '桔推理编辑部',
+    content: '蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女蒙轻纱的处女',
+    star: '/images/icon/star/star-3.png',
+    score: '6.2',
+    favour_image: '/images/icon/chat.png',
+    favour_count: 81,
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    name: '和平婚姻',
+    date:'2019年5月15日',
+    type: '本格',
+    number: '5',
+    time: '3小时',
+    img: '/images/carousel/post004.jpg',
+    avatar: '/images/avatar/coderjuzi.png',
+    author: '桔推理编辑部',
+    content: '和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻和平婚姻',
+    star: '/images/icon/star/star-4.png',
+    score: '7.6',
+    favour_image: '/images/icon/chat.png',
+    favour_count: 102,
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    name: '苏荷',
+    date:'2018年11月19日',
+    type: '本格',
+    number: '5',
+    time: '4小时',
+    img: '/images/carousel/post011.jpg',
+    avatar: '/images/avatar/coderjuzi.png',
+    author: '桔推理编辑部',
+    content: '苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷苏荷',
+    star: '/images/icon/star/star-4.png',
+    score: '7.9',
+    favour_image: '/images/icon/chat.png',
+    favour_count: 258,
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+];
+  
+module.exports = {home_data};
